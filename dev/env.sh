8c1580fdaf5327042cfc991f5d9ac53cefe27eff
ushaarava <usha.arava@gmail.com>
2020-11-29 10:28:36 +1100

    add file

--- dev/env.sh
+++ dev/env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-image=ami-044c46b1952ad5861
-insTyp=t2.micro
-subnet=subnet-77e8822f
-SGs=sg-057e733eb6e851100
+Image=ami-044c46b1952ad5861
+InstanceType=t2.micro
+Subnets=subnet-77e8822f
+SecurityGroup=sg-057e733eb6e851100
 KeyPair=ush11
 count=1
 region=ap-southeast-2
