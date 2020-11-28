d45cb0beb16d4fe16428843c78746061f178d656
ushaarava <usha.arava@gmail.com>
2020-11-29 10:35:13 +1100

    *

--- dev/env.sh
+++ dev/env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 Image=ami-044c46b1952ad5861
-InstanceType=t2.micro
+Instancetype=t2.micro
 Subnets=subnet-77e8822f
 SecurityGroup=sg-057e733eb6e851100
-KeyPair=ush11
-count=1
+keypair=ush11
+#count=1
 region=ap-southeast-2
